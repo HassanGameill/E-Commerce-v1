c47a79c2273b2a85599190e21ce38637d5a8e1fe
HassanGameill <hasanaboalion@gmail.com>
2024-06-29 12:36:54 +0200

    first commit

---
+++ src/Components/C-Pages-UI/Cart/useCart.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {useEffect, useCallback} from 'react'
+import {useAppDispatch, useAppSelector} from '../../../RTK-STORE/hooks'
+import { 
+  addGetProductsByItems,
+  cleanCartProductsFullInfo,
+  cartItemsChangeQuantity,
+  cartItemRemove
+} from "../../../RTK-STORE/cart/cartSlice";
+
+
+
+
+const useCart = () => {
+  
+  const dispatch = useAppDispatch();
+  const { items, productsFullInfo, loading, error } = useAppSelector(
+    (state) => state.cart
+  );
+  
+  
+  
+  useEffect(() => {
+   const promise = dispatch(addGetProductsByItems());
+    return () => {
+      dispatch(cleanCartProductsFullInfo())
+      promise.abort();
+    }
+  }, [dispatch]);
+
+  const products = productsFullInfo.map((el) => ({
+    ...el,
+    quantity: items[el.id],
+  }));
+
+
+
+  // cahnge Quantity handler 
+  const changeQuantityHandler = useCallback((id: number, quantity: number) => {
+    dispatch(cartItemsChangeQuantity({id, quantity}))
+  }, [dispatch]);
+  
+  
+  // Removig For Item Cart 
+  const removeItemHandler = useCallback((id: number) => {
+    dispatch(cartItemRemove(id))
+  }, [dispatch])
+
+
+  return {loading, error, productsFullInfo, changeQuantityHandler, removeItemHandler,products }
+}
+
+export default useCart
---
+++ src/Pages/Cart/Cart.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CartRender from "../../Components/C-Pages-UI/Cart/CartRender"
+
+
+const Cart = () => {
+  return (
+    <div className="">
+      <CartRender />
+    </div>
+    
+  )
+}
+
+export default Cart
